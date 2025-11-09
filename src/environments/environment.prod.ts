ac6264e2b81e3be684458ebb50ec02d778afe285
PatricioPonce28 <geanotoponce95@gmail.com>
2025-11-09 16:38:32 -0500

    Commit de cambios para deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyCEKPhpkPdWGtLFxJXiJx5WzNAc8u8qhVU",
-    authDomain: "bb-3abd4.firebaseapp.com",
-    databaseURL: "https://bb-3abd4-default-rtdb.firebaseio.com",
-    projectId: "bb-3abd4",
-    storageBucket: "bb-3abd4.firebasestorage.app",
-    messagingSenderId: "713537563741",
-    appId: "1:713537563741:web:729d0c8aebe201e221b1b4",
-    measurementId: "G-GLN4KW5CJZ"
-  }, 
-  nasaApiKey: 'DEMO_KEY'
+    apiKey: "AIzaSyAhP_aikBtclr38KwFGNJ76SVOPW5OCvpA",
+    authDomain: "repasoapis.firebaseapp.com",
+    projectId: "repasoapis",
+    storageBucket: "repasoapis.firebasestorage.app",
+    messagingSenderId: "111245602707",
+    appId: "1:111245602707:web:21e7a2d36415b3d5604266"
+  },
+ 
 };
